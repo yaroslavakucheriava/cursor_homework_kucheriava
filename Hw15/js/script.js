a4cfc084cbb9e9e499e25f1b4683034b9db9d0fe
yaroslavakucheriava <yaroslavakucheriava.gmail.com>
2022-07-21 20:49:34 +0300

    Hw15

---
+++ Hw15/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.review-slider').slick({
+       infinite: true,
+       slidesToShow: 1,
+       slidesToScroll: 1,
+       dots: false,
+       speed: 300,
+       arrows: true,
+     
+    });
+   });
