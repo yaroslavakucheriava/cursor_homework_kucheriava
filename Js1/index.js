e14df02c863ec5521e78bb4245f89379ab9bf15f
yaroslavakucheriava <94495023+yaroslavakucheriava@users.noreply.github.com>
2022-08-31 19:42:23 +0300

    Update index.js

--- Js1/index.js
+++ Js1/index.js
@@ -6,35 +6,35 @@ let peachPrice = 15.678,
     
 
 // максимальне число    
-    console.log(`Максимальна ціна: ${Math.max(peachPrice, grapePrice, mangoPrice)}`);
+console.log(`Максимальна ціна: ${Math.max(peachPrice, grapePrice, mangoPrice)}`);
 
 // мінімальне число
-    console.log(`Мінімальна ціна: ${Math.min(peachPrice, grapePrice, mangoPrice)}`);
+console.log(`Мінімальна ціна: ${Math.min(peachPrice, grapePrice, mangoPrice)}`);
 
 // вартість всіх товарів
 let price = peachPrice + grapePrice + mangoPrice;
-    console.log(`Вартість всіх товарів: ${price}`);    
+console.log(`Вартість всіх товарів: ${price}`);    
     
 // округлення вартості товарів в меншу сторону    
 let peachValue = Math.floor(peachPrice),
     grapeValue = Math.floor(grapePrice),
     mangoValue = Math.floor(mangoPrice),
     value = peachValue + grapeValue + mangoValue;
-    console.log(`Округлена вартість до меншого: ${value}`);
+console.log(`Округлена вартість до меншого: ${value}`);
 
 // сума товарів округлена до сотень    
-    console.log(`Округлена вартість до сотень: ${Math.round(value/100)*100}`);
+console.log(`Округлена вартість до сотень: ${Math.round(value/100)*100}`);
      
 // boolean
-    console.log(`Вартість товарів - парне число: ${value % 2 === 0}`);
-    console.log(`Вартість товарів - непарне число: ${value % 2 === 1}`);
+console.log(`Вартість товарів - парне число: ${value % 2 === 0}`);
+console.log(`Вартість товарів - непарне число: ${value % 2 === 1}`);
 
 // сума решти, при оплаті всіх товарів сумою 500   
-    console.log(`Сума решти при оплаті всіх товарів з 500, без округлення: ${500-price}`);
+console.log(`Сума решти при оплаті всіх товарів з 500, без округлення: ${500-price}`);
 
 // середнє значення цін, округлене до 2го знаку
-    let average = (price/amount).toFixed(2);
-    console.log(`Середнє арифметичне:${average}`);
+let average = (price/amount).toFixed(2);
+console.log(`Середнє арифметичне:${average}`);
    
 // випадкова знижка
 const minValue = 1;
@@ -48,6 +48,6 @@ console.log(`Сума до оплати зі знижкою: ${price.toFixed() -
 
 // чистий прибуток
 let cost = price.toFixed()/2,
-rebate = (price * randomValue)/100;
+let rebate = (price * randomValue)/100;
 console.log(`Чистий прибуток: ${(cost-rebate).toFixed()}`);
 
