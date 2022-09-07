9d1153ceb024848fa336a57ec3dfd7b7ef25b4bf
yaroslavakucheriava <yaroslavakucheriava.gmail.com>
2022-09-07 23:21:46 +0300

    js2

---
+++ js2/index.js
@@ -0,0 +1,26 @@
+let firstNumber = parseInt(prompt('Enter your first number'), 10);
+
+while (isNaN(firstNumber)) {
+    firstNumber = parseInt(prompt('Enter your number again'), 10)
+}
+
+let lastNumber = parseInt(prompt('Enter your last number'), 100);
+
+while (isNaN(lastNumber) || lastNumber <= firstNumber) {
+    lastNumber = parseInt(prompt('Enter your number again'), 10)
+}
+console.log(firstNumber);
+console.log(lastNumber);
+let missPairNumber = confirm("Do you want to miss pair numbers?");
+console.log(missPairNumber);
+let result = 0;
+
+for (let i = firstNumber; i <= lastNumber; i++) {
+let isEven = i % 2 === 0;
+    if(missPairNumber && isEven) {
+    continue  
+}
+result += i;  
+}
+
+console.log(result)
