9c581b6935f0d2e74ce5982d8bbea51a8cb78117
yaroslavakucheriava <94495023+yaroslavakucheriava@users.noreply.github.com>
2022-09-07 23:38:16 +0300

    Update index.js

--- js2/index.js
+++ js2/index.js
@@ -1,20 +1,21 @@
-let firstNumber = parseInt(prompt('Enter your first number'), 10);
+let firstNumber = parseInt(prompt('Enter your first number'));
 
 while (isNaN(firstNumber)) {
-    firstNumber = parseInt(prompt('Enter your number again'), 10)
+    firstNumber = parseInt(prompt('Enter your number again'))
 }
 
-let lastNumber = parseInt(prompt('Enter your last number'), 100);
+let lastNumber = parseInt(prompt('Enter your last number'));
 
 while (isNaN(lastNumber) || lastNumber <= firstNumber) {
-    lastNumber = parseInt(prompt('Enter your number again'), 100)
+    lastNumber = parseInt(prompt('Enter your number again'))
 }
 console.log(firstNumber);
 console.log(lastNumber);
+
 let missPairNumber = confirm("Do you want to miss pair numbers?");
 console.log(missPairNumber);
-let result = 0;
 
+let result = 0;
 for (let i = firstNumber; i <= lastNumber; i++) {
 let isEven = i % 2 === 0;
     if(missPairNumber && isEven) {
@@ -22,5 +23,4 @@ let isEven = i % 2 === 0;
 }
 result += i;  
 }
-
 console.log(result)
